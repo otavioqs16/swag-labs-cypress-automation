e89706d204cf1fdbe1af8da75dae6b5b443886d4
Otávio Queiroz <otavioqs16@gmail.com>
2025-03-12 12:02:01 -0300

    fix: adjustment in tests

--- cypress/e2e/sauceDemo.cy.js
+++ cypress/e2e/sauceDemo.cy.js
@@ -7,65 +7,65 @@ const setLimits = (limit, addQuantity, removeQuantity) => {
 };
 
 describe("Testes E2E - SauceDemo", () => {
-  // beforeEach(() => {
-  //   cy.login();
+  beforeEach(() => {
+    cy.login();
+  });
+
+  // it("verifica falha ao logar com campos em branco", () => {
+  //   cy.visit(`${Cypress.config("baseUrl")}/index.html`);
+
+  //   // Ambos os campos em branco
+  //   cy.get("input[type='submit']").click();
+  //   cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
+  //   cy.get("h3[data-test='error']")
+  //     .should("be.visible")
+  //     .and("contain", "Username is required");
+
+  //   // Usuário em branco
+  //   cy.get("input[name='password']").type(Cypress.env("user_password"), {
+  //     log: false,
+  //   });
+  //   cy.get("input[type='submit']").click();
+  //   cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
+  //   cy.get("h3[data-test='error']")
+  //     .should("be.visible")
+  //     .and("contain", "Username is required");
+
+  //   // Senha em branco
+  //   cy.get("input[name='password']").clear();
+  //   cy.get("input[name='user-name']").type(Cypress.env("user_name"));
+  //   cy.get("input[type='submit']").click();
+  //   cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
+  //   cy.get("h3[data-test='error']")
+  //     .should("be.visible")
+  //     .and("contain", "Password is required");
   // });
 
-  it.only("verifica falha ao logar com campos em branco", () => {
-    cy.visit(`${Cypress.config("baseUrl")}/index.html`);
+  // it("verifica falha ao logar com user inválido", () => {
+  //   cy.visit(`${Cypress.config("baseUrl")}/index.html`);
 
-    // Ambos os campos em branco
-    cy.get("input[type='submit']").click();
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
-    cy.get("h3[data-test='error']")
-      .should("be.visible")
-      .and("contain", "Username is required");
+  //   cy.get("input[name='user-name']").type("user");
+  //   cy.get("input[name='password']").type(Cypress.env("user_password"), {
+  //     log: false,
+  //   });
+  //   cy.get("input[type='submit']").click();
 
-    // Usuário em branco
-    cy.get("input[name='password']").type(Cypress.env("user_password"), {
-      log: false,
-    });
-    cy.get("input[type='submit']").click();
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
-    cy.get("h3[data-test='error']")
-      .should("be.visible")
-      .and("contain", "Username is required");
+  //   cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
+  //   cy.get("h3[data-test='error']").should("be.visible");
+  // });
 
-    // Senha em branco
-    cy.get("input[name='password']").clear();
-    cy.get("input[name='user-name']").type(Cypress.env("user_name"));
-    cy.get("input[type='submit']").click();
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
-    cy.get("h3[data-test='error']")
-      .should("be.visible")
-      .and("contain", "Password is required");
-  });
+  // it("verifica falha ao logar com senha inválida", () => {
+  //   cy.visit(`${Cypress.config("baseUrl")}/index.html`);
 
-  it("verifica falha ao logar com user inválido", () => {
-    cy.visit(`${Cypress.config("baseUrl")}/index.html`);
+  //   cy.get("input[name='user-name']").type(Cypress.env("user_name"));
+  //   cy.get("input[name='password']").type("senha", {
+  //     log: false,
+  //   });
+  //   cy.get("input[type='submit']").click();
 
-    cy.get("input[name='user-name']").type("user");
-    cy.get("input[name='password']").type(Cypress.env("user_password"), {
-      log: false,
-    });
-    cy.get("input[type='submit']").click();
-
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
-    cy.get("h3[data-test='error']").should("be.visible");
-  });
-
-  it("verifica falha ao logar com senha inválida", () => {
-    cy.visit(`${Cypress.config("baseUrl")}/index.html`);
-
-    cy.get("input[name='user-name']").type(Cypress.env("user_name"));
-    cy.get("input[name='password']").type("senha", {
-      log: false,
-    });
-    cy.get("input[type='submit']").click();
-
-    cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
-    cy.get("h3[data-test='error']").should("be.visible");
-  });
+  //   cy.url().should("eq", `${Cypress.config("baseUrl")}/index.html`);
+  //   cy.get("h3[data-test='error']").should("be.visible");
+  // });
 
   it("verifica sucesso ao logar", () => {
     // Assert - Verificação (Garante o resultado esperado)
