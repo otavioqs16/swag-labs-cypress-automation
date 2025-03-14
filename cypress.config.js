9546ab71ec39648b44ecd12a0396df008f6da0b6
Otávio Queiroz <otavioqs16@gmail.com>
2025-03-14 15:14:17 -0300

    feat: add sequencialism and specifying the jobs

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,11 @@ module.exports = defineConfig({
   projectId: "eooi8w",
   e2e: {
     baseUrl: "https://www.saucedemo.com/v1",
+    specPattern: [
+      '**/login.cy.js',
+      '**/productsPage.cy.js',
+      '**/sideMenu.cy.js',
+      '**/cartAndShop.cy.js'
+    ]
   },
 });
